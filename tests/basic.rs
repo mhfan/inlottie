a652235ebf3625071972831346cd87c029e76538
MeiHui FAN <mhfan@ustc.edu>
2023-09-27 16:48:59 +0800

    traversely test all lottie json files

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,27 +1,37 @@
 
-use std::{fs, io::Error};
+use std::fs::File;
 use inlottie::schema::{GradientFill, Stroke, Transform, Animation};
 
-#[test] pub fn test_transform_complex() -> Result<(), Error> {
-    let file = fs::File::open("fixtures/unit/segments/transform_complex.json")?;
-    let d = &mut serde_json::Deserializer::from_reader(file);
-    let _: Transform = serde_path_to_error::deserialize(d).unwrap(); Ok(())
+#[test] pub fn parse_simple() -> Result<(), Box<dyn std::error::Error>> {
+    for path in glob::glob("fixtures/unit/simple/*.json")?.filter_map(Result::ok) {
+        eprintln!("Parsing {}", path.display()); //let path = path?;
+        let _: Animation = serde_path_to_error::deserialize(
+            &mut serde_json::Deserializer::from_reader(File::open(path)?))?;
+    }
+    for path in glob::glob("fixtures/ui/**/*.json")?.filter_map(Result::ok) {
+        eprintln!("Parsing {}", path.display()); //let path = path?;
+        let _: Animation = serde_path_to_error::deserialize(
+            &mut serde_json::Deserializer::from_reader(File::open(path)?))?;
+    }   Ok(())
 }
 
-#[test] pub fn test_stroke() -> Result<(), Error> {
-    let file = fs::File::open("fixtures/unit/segments/stroke.json")?;
-    let d = &mut serde_json::Deserializer::from_reader(file);
-    let _: Stroke = serde_path_to_error::deserialize(d).unwrap(); Ok(())
-}
+#[test] pub fn parse_segments() -> Result<(), Box<dyn std::error::Error>> {
+    let path_base = "fixtures/unit/segments";
 
-#[test] pub fn test_gradient_fill() -> Result<(), Error> {
-    let file = fs::File::open("fixtures/unit/segments/gradient_fill.json")?;
-    let d = &mut serde_json::Deserializer::from_reader(file);
-    let _: GradientFill = serde_path_to_error::deserialize(d).unwrap(); Ok(())
-}
+    let file_path = format!("{}/transform_complex.json", path_base);
+    eprintln!("Parsing {file_path}");
+    let _: Transform = serde_path_to_error::deserialize(
+        &mut serde_json::Deserializer::from_reader(File::open(file_path)?))?;
 
-#[test] fn test_bouncy_ball_example() -> Result<(), Error> {
-    let file = fs::File::open("fixtures/ui/lottie-ios-samples/Nonanimating/FirstText.json")?;
-    let d = &mut serde_json::Deserializer::from_reader(file);
-    let _: Animation = serde_path_to_error::deserialize(d).unwrap(); Ok(())
+    let file_path = format!("{}/gradient_fill.json", path_base);
+    eprintln!("Parsing {file_path}");
+    let _: GradientFill = serde_path_to_error::deserialize(
+        &mut serde_json::Deserializer::from_reader(File::open(file_path)?))?;
+
+    let file_path = format!("{}/stroke.json", path_base);
+    eprintln!("Parsing {file_path}");
+    let _: Stroke = serde_path_to_error::deserialize(
+        &mut serde_json::Deserializer::from_reader(File::open(file_path)?))?;
+
+    Ok(())
 }
